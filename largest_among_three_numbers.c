1b1ed6fcf0da009e386d70967a15f8e2ec297399
aryyah22 <aryanandaretheesh@gmail.com>
2026-09-15 15:21:31 +0530

    Update largest_among_three_numbers.c

--- largest_among_three_numbers.c
+++ largest_among_three_numbers.c
@@ -2,19 +2,19 @@
 int main()
 {
 int a,b,c;
-printf("Enter three numbers\n");
+printf("Enter three num\n");
 scanf("%d%d%d",&a,&b,&c);
 if(a>b&&a>c)
 {
-printf("The largest number is:%d\n",a);
+printf("The largest num is:%d\n",a);
 }
 else if(b>a&&b>c)
 {
-printf("The largest number is:%d\n",b);
+printf("The largest num is:%d\n",b);
 }
 else
 {
-printf("The largest number is:%d\n",c);
+printf("The largest num is:%d\n",c);
 }
 return 0;
 }
