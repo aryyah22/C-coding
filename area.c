c3ee0c899aaecf9e9326075a31118cdc0ef43c95
aryyah22 <aryanandaretheesh@gmail.com>
2026-09-15 15:18:10 +0530

    Update area.c

--- area.c
+++ area.c
@@ -2,11 +2,11 @@
 #define PI 3.14
 int main()
 {
-int radius;
-printf("Enter the value of radius:\n");
-scanf("%d",&radius);
+int r;
+printf("Enter the value of r:\n");
+scanf("%d",&r);
 float area;
-area=PI*radius*radius;
+area=PI*r*r;
 printf("The area of the circle is:%.2f\n",area);
 return 0;
 }
